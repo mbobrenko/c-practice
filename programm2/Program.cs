8fb77e6ac45a5d922357d5a94a0a6cf2d8c70af4
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-17 15:35:44 +0300

    programm2 added

---
+++ programm2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Написать программу, которая на вход принимает число а выдает квадрат
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number);
+int sqr = number * number;
+Console.Write(number + " ");
+Console.WriteLine(sqr);
