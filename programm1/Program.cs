8ea421015793e5e3e56d1235e378203b29f05704
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-17 15:29:28 +0300

    programm1 added

---
+++ programm1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Написать прогрвмму, которая на вход принимает число а выдает квадрат
+int number = new Random().Next(0,10);
+Console.WriteLine(number);
+int sqr = number * number;
+Console.Write(number + " ");
+Console.WriteLine(sqr);
