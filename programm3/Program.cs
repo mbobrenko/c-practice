8efc30b391866a9287411d68a806924db1b2fbd1
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-19 13:04:10 +0300

    programm3 added

---
+++ programm3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Написать программу, которая на вход принимает два числа и проверяет является ли первое число квдратом второго*/
+Console.Write("Enter first number: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+int sqr = numberTwo*numberTwo;
+if(numberOne == sqr)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+/*Написать программу которая выдает по цифре дни недели*/
