e7deba9efafda7df111db49d8114661fe109a97c
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-19 14:38:07 +0300

    programm6 added

---
+++ programm6/Program.cs
@@ -0,0 +1,11 @@
+﻿//Пользователь ввел число вывести послднюю цифру этого числа
+
+Console.WriteLine("Enter the number: ");
+int number=Convert.ToInt32(Console.ReadLine());
+if(number>99)
+{if(number<1000)
+    {
+    int lastDigit = number % 10;
+    Console.WriteLine(lastDigit);
+    }
+}
