af0e680b7b7e5bdee6f282b2589110e16925c741
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-19 14:37:32 +0300

    programm5 added

---
+++ programm5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу которая на вход принимает число и выдает его квадрат
+Console.WriteLine("Enter the number: ");
+int number=Convert.ToInt32(Console.ReadLine());
+int negativeNumber = -number;
+while(negativeNumber <= number)
+{
+    Console.Write(negativeNumber + " ");
+    negativeNumber++;
+}
