8550d9e92a3353b43b1ccf26e9a22ad5c4202487
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-19 13:23:25 +0300

    programm4 added

---
+++ programm4/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Написать программу которая выдает по цифре дни недели*/
+Console.Write("Enter the number: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+//Convert.ToInt32 - конвертировать число в строку//
+if(dayNumber == 1)
+{
+    Console.WriteLine("Monday");
+  
+}
+if(dayNumber == 2)
+{
+    Console.WriteLine("Tuesday");
+  
+}
+if(dayNumber == 3)
+{
+    Console.WriteLine("Wednesday");
+  
+}
+if(dayNumber == 4)
+{
+    Console.WriteLine("Thursday");
+  
+}
+if(dayNumber == 5)
+{
+    Console.WriteLine("Friday");
+  
+}
+if(dayNumber == 6)
+{
+    Console.WriteLine("Saturday");
+  
+}
+if(dayNumber == 7)
+{
+    Console.WriteLine("Sunday");
+  
+}
