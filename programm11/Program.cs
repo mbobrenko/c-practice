84c52cbe0c0cfacf3c422503f3641892b9c7bd0d
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-22 14:27:11 +0300

    programm11 added

---
+++ programm11/Program.cs
@@ -0,0 +1,16 @@
+﻿int number =45879554;
+int searchDigit=4;
+int count = 0; 
+int temp =number; 
+while (temp > 0)
+{
+    temp/=10;
+    count++;
+}
+
+while (count > searchDigit)
+{
+    number/=10;
+    count --;
+}
+Console.WriteLine(number%10);
