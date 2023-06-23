d2bab5eb6021fb7da7cef5d0ba0a71db3d60a022
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-23 10:00:15 +0300

    programm17-18  added

---
+++ programm18/Program.cs
@@ -0,0 +1,17 @@
+﻿// Проверить полидром 
+int number = 12321; 
+
+int first= number/10000;
+int second =number/1000 % 10;
+int third = number /100 %10;
+int four = number /10 % 10;
+int fifth = number %10;
+
+if (first == fifth && second ==four)
+{
+Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
