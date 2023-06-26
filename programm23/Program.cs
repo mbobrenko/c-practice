827623b5d12b7066bf2a49a1b3f9e3db9a9c3f1a
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-26 16:18:44 +0300

    programm23-25  added

---
+++ programm23/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, котороая принимает на вход исло. и выдает сумму цифр в числе
+/*string stringNumber = Console.ReadLine();
+int sum = 0;
+
+for(int i = 0; i < stringNumber.Length; i++)
+{
+string digit = Convert.ToString(stringNumber[i]);
+sum += Convert.ToInt32(digit);
+}
+
+Console.WriteLine(sum);*/
+
+int number = Convert.ToInt32(Console.ReadLine());
+int countDigit = 0;
+int sum = 0;
+
+int temp = number;
+while(temp > 0)
+{
+countDigit++;
+temp /= 10;
+}
+
+int[] numbers = new int[countDigit];
+int index = 0;
+while(number > 0)
+{
+numbers[index++] = number % 10;
+number /= 10;
+}
+
+for(int i = 0; i < numbers.Length; i++)
+{
+sum += numbers[i];
+}
+
+Console.WriteLine(sum);
---
+++ programm24/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Написать программу которая задает массив из N элементов и выводит их на экран
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers, 0, 10);
+WriteArray(numbers);
+
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(min, max);
+}
+}
+
+void WriteArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+}
