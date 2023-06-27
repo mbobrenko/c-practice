3d68f2f195771b86fb8141e05a7bdaeab1d939d0
Marina Bobrenko <m.bobrenko@gmail.com>
2023-06-27 11:35:07 +0300

    Added programm24-26

---
+++ programm26/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.[-4, -8, 8, 2] -> [4, 8, -8, -2]
+int size = ReadInt("Введите размер массива: ");
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers, -9, 9);
+WriteArray(numbers);
+
+for(int i = 0; i < numbers.Length; i++)
+{
+numbers[i] = -numbers[i];
+}
+
+WriteArray(numbers);
+
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(min, max + 1);
+}
+}
+
+void WriteArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
---
+++ programm27/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.4; массив [6, 7, 19, 345, 3] -> нет
+//3; массив [6, 7, 19, 345, 3] -> да
+
+int size = ReadInt("Введите размер массива: ");
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers, 1, 10);
+WriteArray(numbers);
+
+int searchNumber = ReadInt("Введите искомое число: ");
+
+for(int i = 0; i < numbers.Length; i++)
+{
+if (searchNumber == numbers[i])
+{
+Console.WriteLine("Да");
+break;
+}
+else if (i == numbers.Length - 1)
+{
+Console.WriteLine("Нет");
+}
+}
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(min, max + 1);
+}
+}
+
+void WriteArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
