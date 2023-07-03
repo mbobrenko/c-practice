0b94121f95d72a758fad889f664f158339cf867e
Marina Bobrenko <m.bobrenko@gmail.com>
2023-07-03 12:23:02 +0300

    Added programm 46-47

---
+++ programm46/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+
+int sum=0;
+
+for(int i = 0; i < numbers.GetLength(0); i++)
+{
+for(int j = 0; j < numbers.GetLength(1); j++)
+{
+sum += numbers[i, j];
+}
+Console.WriteLine(sum / numbers.GetLength(0));
+sum=0;
+}
+
+
+void FillMatrixRandomNumbers(int[,] array)
+{
+for(int i = 0; i < array.GetLength(0); i++)
+{
+for(int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = new Random().Next(1, 10);
+}
+}
+}
+
+void WriteMatrix(int[,] array)
+{
+for(int i = 0; i < array.GetLength(0); i++)
+{
+for(int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write(array[i, j] + " ");
+}
+Console.WriteLine();
+}
+Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
