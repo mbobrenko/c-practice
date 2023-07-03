2e8197af409f168f480a4549cac152de1b02897b
Marina Bobrenko <m.bobrenko@gmail.com>
2023-07-03 12:46:20 +0300

    Added programm 47-48improved

---
+++ programm48/Program.cs
@@ -0,0 +1,50 @@
+﻿//  Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+if(rows != columns)
+{
+Console.WriteLine("Операция невозможна");
+return;
+}
+int[,] numbers = new int[rows, columns];
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+
+for(int i = 0; i < numbers.GetLength(0); i++)
+{
+for(int j = i; j < numbers.GetLength(1); j++)
+{
+int temp = numbers[i, j];
+numbers[i, j] = numbers[j, i];
+numbers[j, i] = temp;
+}
+}
+WriteMatrix(numbers);
+void FillMatrixRandomNumbers(int[,] array)
+{
+for(int i = 0; i < array.GetLength(0); i++)
+{
+for(int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = new Random().Next(1, 10);
+}
+}
+}
+void WriteMatrix(int[,] array)
+{
+for(int i = 0; i < array.GetLength(0); i++)
+{
+for(int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write(array[i, j] + " ");
+}
+Console.WriteLine();
+}
+Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
